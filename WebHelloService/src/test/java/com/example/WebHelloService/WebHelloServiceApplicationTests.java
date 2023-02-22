7581da17498047bdacf94e2337cafc23ee74dcef
Siva Kishore Chevuru <sivakishore.chevuru@gmail.com>
2023-02-21 20:47:18 -0500

    Web GUI Standard App pushed

--- WebHelloService/src/test/java/com/example/WebHelloService/WebHelloServiceApplicationTests.java
+++ WebHelloService/src/test/java/com/example/WebHelloService/WebHelloServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.WebHelloService;
 
+import com.example.webhello.WebHelloServiceApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = WebHelloServiceApplication.class)
 class WebHelloServiceApplicationTests {
 
 	@Test
